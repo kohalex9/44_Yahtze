b5ab9219888b3dff6cbf48342608ad6ebc2eb82b
kohalex9 <kohalex9@gmail.com>
2017-08-17 15:28:24 +0800

    First Commit, with a dieLabel and DieLabelcLASS

---
+++ 44_Yahtze/DieLabel.swift
@@ -0,0 +1,19 @@
+//
+//  DieLabel.swift
+//  44_Yahtze
+//
+//  Created by Alex Koh on 17/08/2017.
+//  Copyright © 2017 AlexKoh. All rights reserved.
+//
+
+import UIKit
+
+class DieLabel: UILabel {
+    
+    
+    func actionsToCarryOutAfterDieBtnTapped() {
+        print(self.text)
+    }
+    
+    
+}
